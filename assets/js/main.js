052bd11adc4543a87b26aa532a0933c71f42b11f
donkyshon <74634255+donkyshon@users.noreply.github.com>
2022-11-11 08:13:04 +0300

    ico

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const button = document.querySelector(".preloader-content_button");
 let animInterval;
 let loadInterval;
 
-const videoId = "Dqi9W7Z7r3o"
+const videoId = "g8V0sHQG8sE"
 YT.ready(_=>{
     window.player = new YT.Player("player", {
         videoId: videoId,
