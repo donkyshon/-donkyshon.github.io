fbe339241b72cceea3ab45b8bea271cd5c7b5df3
unknown <donkyshon@gmail.com>
2023-05-25 14:56:55 +0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const button = document.querySelector(".preloader-content_button");
 let animInterval;
 let loadInterval;
 
-const videoId = "VEizKmZlUAw"
+const videoId = "8UPUPDo20nM"
 YT.ready(_=>{
     window.player = new YT.Player("player", {
         videoId: videoId,
